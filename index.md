3d67272584fb37589a1d1c8e5a0425e03bcff530
Jo Hund <jhund@clearcove.ca>
2013-05-06 22:56:10 -0700

    Added intro graphic and made graphics responsive

--- index.md
+++ index.md
@@ -3,65 +3,82 @@ layout: default
 ---
 
 <div class="page-header">
-  <h1>Raphael.handdrawn.js</h1>
+  <h1>Handdrawn.js</h1>
 </div>
 
 A RaphaelJS plugin to give your vector graphics a hand drawn style. Great for
 sketch diagrams, charts and illustrations.
 
+<div id="handdrawnIntro" class="rPaper img-polaroid"></div>
 
-<h3>drawnLine</h3>
-<p>Command: <code>paper.drawnLine(x, y, x2, y2, wobble)</code></p>
-<p>The example below renders a series of horizontal lines with increasing wobble:</p>
-<div id="handdrawnLine" style="height: 300px;"></div>
+### drawnLine
 
+<pre>paper.drawnLine(x, y, x2, y2, wobble)</pre>
 
+The example below renders a series of horizontal lines with increasing wobble:
 
-<h3>drawnRect</h3>
-<p>Command: <code>paper.drawnRect(x, y, x2, y2, wobble)</code></p>
-<p>The example below renders a series of rectangles with increasing wobble:</p>
-<div id="handdrawnRect" style="height: 300px;"></div>
+<div id="handdrawnLine" class="rPaper img-polaroid"></div>
 
 
 
-<h3>drawnCircularArc</h3>
-<p>Command: <code>paper.drawnCircularArc(centerX, centerY, radius, startAngle, endAngle)</code></p>
-<p>
-  The example below renders a series of circular arcs. The wobble on circular
-  arcs is a function of the radius.
-</p>
-<div id="handdrawnCircularArc" style="height: 300px;"></div>
+### drawnRect
+
+<pre>paper.drawnRect(x, y, x2, y2, wobble)</pre>
+
+The example below renders a series of rectangles with increasing wobble:
+
+<div id="handdrawnRect" class="rPaper img-polaroid"></div>
 
 
 
-<h3>drawnRegularPolygon</h3>
-<p>Command: <code>paper.drawnRegularPolygon(centerX, centerY, radius, numberOfSides, wobble)</code></p>
-<p>The example below renders a grid of regular polygons with random number of sides.</p>
-<div id="handdrawnRegularPolygon" style="height: 300px;"></div>
+### drawnCircularArc
+
+<pre>paper.drawnCircularArc(centerX, centerY, radius, startAngle, endAngle)</pre>
+
+The example below renders a series of circular arcs. The wobble on circular
+arcs is a function of the radius.
+
+<div id="handdrawnCircularArc" class="rPaper img-polaroid"></div>
 
 
 
-<h3>drawnCircle</h3>
-<p>Command: <code>paper.drawnCircle(centerX, centerY, radius, wobble)</code></p>
-<p>
-  The hand drawn diagrams go well with a handwriting font as shown in this example.
-  This example also shows that closed handdrawn shapes can have a fill attribute.
-</p>
-<div id="handdrawnCircle" style="height: 300px;"></div>
+### drawnRegularPolygon
+
+<pre>paper.drawnRegularPolygon(centerX, centerY, radius, numberOfSides, wobble)</pre>
+
+The example below renders a grid of regular polygons with random number of sides.
+
+<div id="handdrawnRegularPolygon" class="rPaper img-polaroid"></div>
 
 
 
-<h3>Random number distributions</h3>
-<p>
-  This chart compares one uniform and two normal distributions for random numbers.
-  Currently I use the uniform distribution, but I'm exploring normal distributions
-  for a more natural look.
-</p>
-<div id="rnPaper" style="height: 300px;"></div>
+### drawnCircle
+
+<pre>paper.drawnCircle(centerX, centerY, radius, wobble)</pre>
+
+The hand drawn diagrams go well with a handwriting font as shown in this example.
+This example also shows that closed handdrawn shapes can have a fill attribute.
+
+<div id="handdrawnCircle" class="rPaper img-polaroid"></div>
+
+
+### Responsive design with Raphael
+
+The SVG graphics on this page are responsive, i.e. they shrink as the
+viewport gets smaller. I followed this 
+[recipe for responsive SVG graphics](http://jsfiddle.net/AUNwC/44/)
+
+### Random number distributions
+
+This chart compares one uniform and two normal distributions for random numbers.
+Currently I use the uniform distribution, but I'm exploring normal distributions
+for a more natural look.
+
+<div id="rnPaper" class="rPaper img-polaroid"></div>
 
 ----
 
-More info on raphael.handdrawn.js
+More info
 ---------
 
 ### Dependencies
@@ -82,6 +99,10 @@ More info on raphael.handdrawn.js
 * [Source code (github)](https://github.com/jhund/raphael.handdrawn.js)
 * [Issues](https://github.com/jhund/raphael.handdrawn.js/issues)
 
+
+### Credits
+
+This plugin is based on https://github.com/the55/implements55
 
 
 ### License
--- javascripts/raphael.handdrawn.demo.js
+++ javascripts/raphael.handdrawn.demo.js
@@ -2,19 +2,62 @@ var hdWidth = 630,
     hdHeight = 300;
 
 $(function(){
+
+  var paperIntro = Raphael('handdrawnIntro', hdWidth, hdHeight);
+  paperIntro.setViewBox(0, 0, hdWidth, hdHeight, true);
+  paperIntro.setSize('100%', '100%');
+  paperIntro.drawnCircle(
+    470, 170, 100, 5
+  ).attr({
+    stroke: "#000", fill: "#ff0", "fill-opacity": 0.2
+  });
+  paperIntro.text(
+    470,
+    170,
+    "with\nRaphaelJS"
+  ).attr({
+    "font-family": "'Shadows Into Light', cursive",
+    "font-size": 24
+  });
+  paperIntro.drawnRect(
+    20, 20, 380, 180, 5
+  ).attr({
+    stroke: "#000", fill: "#f00", "fill-opacity": 0.2
+  });
+  paperIntro.text(
+    200,
+    100,
+    "Hand drawn\nSVG graphics"
+  ).attr({
+    "font-family": "'Shadows Into Light', cursive",
+    "font-size": 48
+  });
+  paperIntro.text(
+    255,
+    290,
+    "* reload the page to see how the graphic looks different every time"
+  ).attr({
+    "font-family": "'Shadows Into Light', cursive",
+    "font-size": 18
+  });
+
   var paperLine = Raphael('handdrawnLine', hdWidth, hdHeight);
-  for(var i = 1; i < (hdHeight - 10); i += 11) {
+  paperLine.setViewBox(0, 0, hdWidth, hdHeight, true);
+  paperLine.setSize('100%', '100%');
+  for(var i = 20; i < (hdHeight - 10); i += 11) {
     paperLine.drawnLine(
       20,
-      i + 2.5,
+      i + 2,
       hdWidth - 20,
-      i + 2.5,
+      i + 2,
       (i / 30)
     ).attr({ stroke: "#999" });
   };
 
   var paperRect = Raphael("handdrawnRect", hdWidth, hdHeight);
-  for(var i = 1; i < hdWidth - 100; i+= 50) {
+  paperRect.setViewBox(0, 0, hdWidth, hdHeight, true);
+  paperRect.setSize('100%', '100%');
+  for(var i = 20; i < hdWidth - 100; i+= 50) {
     paperRect.drawnRect(
       100 - i/10,
       70 - i/10,
@@ -25,6 +68,8 @@ $(function(){
   };
 
   var paperCircularArc = Raphael('handdrawnCircularArc', hdWidth, hdHeight);
+  paperCircularArc.setViewBox(0, 0, hdWidth, hdHeight, true);
+  paperCircularArc.setSize('100%', '100%');
   for(var i = 0; i < 75; i++) {
     var r = hdWidth + Raphael.randomize(100),
         cx = hdWidth/2
@@ -35,6 +80,8 @@ $(function(){
   };
 
   var paperRegularPolygon = Raphael('handdrawnRegularPolygon', hdWidth, hdHeight);
+  paperRegularPolygon.setViewBox(0, 0, hdWidth, hdHeight, true);
+  paperRegularPolygon.setSize('100%', '100%');
   for(var i = 0; i < hdWidth+20; i+=100) {
     for(var j=0; j < hdHeight+20; j +=100) {
       paperRegularPolygon.drawnRegularPolygon(
@@ -48,6 +95,8 @@ $(function(){
   };
 
   var paperCircle = Raphael('handdrawnCircle', hdWidth, hdHeight);
+  paperCircle.setViewBox(0, 0, hdWidth, hdHeight, true);
+  paperCircle.setSize('100%', '100%');
   paperCircle.drawnCircle(
     hdWidth/2, hdHeight/2, 75, 5
   ).attr({
@@ -62,13 +111,17 @@ $(function(){
     "font-size": 18
   });
 
-  var rnPaper = Raphael('rnPaper', hdWidth, hdHeight),
+  var width = 350,
+      height = 200,
+      rnPaper = Raphael('rnPaper', width, height),
       uniform = {},
       normal1 = {},
       normal2 = {},
       range = 100,
       numSamples = 5000,
       xOffset;
+  rnPaper.setViewBox(0, 0, width, height, true);
+  rnPaper.setSize('100%', '100%');
   // initialize histogram containers
   for(var i = 0; i < (range * 2); i++) {
     uniform[i] = 0;
@@ -84,22 +137,22 @@ $(function(){
   // render uniform distribution
   xOffset = 0 * (range + 20);
   for(var i = 0; i < (range * 2); i++) {
-    rnPaper.path('M' + (i + xOffset) + ' ' + hdHeight + 'L' + (i + xOffset) + ' ' + (hdHeight - uniform[i])).translate(0.5, 0.5);
+    rnPaper.path('M' + (i + xOffset) + ' ' + height + 'L' + (i + xOffset) + ' ' + (height - uniform[i])).translate(0.5, 0.5);
   };
-  rnPaper.path('M' + xOffset + ' 0L' + xOffset + ' ' + hdHeight).attr({ stroke: "#f00" }).translate(0.5, 0.5);
-  rnPaper.path('M' + (xOffset + range) + ' 0L' + (xOffset + range) + ' ' + hdHeight).translate(0.5, 0.5).attr({ stroke: "#f00" });
+  rnPaper.path('M' + xOffset + ' 0L' + xOffset + ' ' + height).attr({ stroke: "#f00" }).translate(0.5, 0.5);
+  rnPaper.path('M' + (xOffset + range) + ' 0L' + (xOffset + range) + ' ' + height).translate(0.5, 0.5).attr({ stroke: "#f00" });
   // render normal1 distribution
   xOffset = 1 * (range + 20);
   for(var i = 0; i < (range * 2); i++) {
-    rnPaper.path('M' + (i + xOffset) + ' ' + hdHeight + 'L' + (i + xOffset) + ' ' + (hdHeight - normal1[i])).translate(0.5, 0.5);
+    rnPaper.path('M' + (i + xOffset) + ' ' + height + 'L' + (i + xOffset) + ' ' + (height - normal1[i])).translate(0.5, 0.5);
   };
-  rnPaper.path('M' + xOffset + ' 0L' + xOffset + ' ' + hdHeight).attr({ stroke: "#f00" }).translate(0.5, 0.5);
-  rnPaper.path('M' + (xOffset + range) + ' 0L' + (xOffset + range) + ' ' + hdHeight).translate(0.5, 0.5).attr({ stroke: "#f00" });
+  rnPaper.path('M' + xOffset + ' 0L' + xOffset + ' ' + height).attr({ stroke: "#f00" }).translate(0.5, 0.5);
+  rnPaper.path('M' + (xOffset + range) + ' 0L' + (xOffset + range) + ' ' + height).translate(0.5, 0.5).attr({ stroke: "#f00" });
   // render normal2 distribution
   xOffset = 2 * (range + 20);
   for(var i = 0; i < (range * 2); i++) {
-    rnPaper.path('M' + (i + xOffset) + ' ' + hdHeight + 'L' + (i + xOffset) + ' ' + (hdHeight - normal2[i])).translate(0.5, 0.5);
+    rnPaper.path('M' + (i + xOffset) + ' ' + height + 'L' + (i + xOffset) + ' ' + (height - normal2[i])).translate(0.5, 0.5);
   };
-  rnPaper.path('M' + xOffset + ' 0L' + xOffset + ' ' + hdHeight).attr({ stroke: "#f00" }).translate(0.5, 0.5);
-  rnPaper.path('M' + (xOffset + range) + ' 0L' + (xOffset + range) + ' ' + hdHeight).translate(0.5, 0.5).attr({ stroke: "#f00" });
+  rnPaper.path('M' + xOffset + ' 0L' + xOffset + ' ' + height).attr({ stroke: "#f00" }).translate(0.5, 0.5);
+  rnPaper.path('M' + (xOffset + range) + ' 0L' + (xOffset + range) + ' ' + height).translate(0.5, 0.5).attr({ stroke: "#f00" });
 });
